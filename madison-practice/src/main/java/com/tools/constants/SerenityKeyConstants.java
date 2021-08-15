a3e489edf496aafa7831d5cb0782ebfa4f7d234b
Marc Vlad <marcvlad90@gmail.com>
2021-08-15 14:08:04 +0300

    Delete madison-practice directory

--- madison-practice/src/main/java/com/tools/constants/SerenityKeyConstants.java
+++
@@ -1,6 +0,0 @@
-package com.tools.constants;
-
-public class SerenityKeyConstants {
-    public static final String CART_PRODUCTS_LIST = "Cart Products";
-    public static final String WISHLIST_PRODUCTS_LIST = "Cart Products";
-}
